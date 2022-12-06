21ba05dc33e259bee9429a9998cf413c75b9dda9
adhayan002 <96379455+adhayan002@users.noreply.github.com>
2022-12-06 17:04:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,34 +5,38 @@ var inputvalue=document.querySelector(".inputValue")
 var name1=document.querySelector(".name")
 var desc=document.querySelector(".desc")
 var temp=document.querySelector(".temp")
+
+async function image(){
+    const response = await fetch("https://pixabay.com/api/?key=31607599-89fa6d790daae6d87152ac3d8&per_page=200")
+    const json=await response.json()
+    const imgae=json.hits[Math.round(Math.random() * 200)].largeImageURL
+    document.body.style.backgroundImage=`url(${imgae})`
+}
+
+
+async function WeatherApp(){
+    const response=await fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputvalue.value+
+    "&appid="+apikey+"&units=metric")
+    if(!response.ok){
+        alert("Error: Enter a valid city name")
+    }
+    else{
+        image()
+        const json=await response.json()
+        var nameValue=json['name']
+        var tempValue=json["main"]['temp']
+        var descValue=json["weather"][0]['main']
+        var iconValue=json["weather"][0]['icon']
+        name1.innerHTML=nameValue
+        temp.innerHTML=Math.round(tempValue)+"°C"
+        desc.innerHTML=descValue
+        document.querySelector(".icon").src="http://openweathermap.org/img/wn/"+iconValue+"@2x.png"
+    }
+
+}
     
 button.addEventListener("click",function(){
-    fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputvalue.value+
-    "&appid="+apikey+"&units=metric")
-        .then(Response=>Response.json())
-        .then(data=>{
-            var nameValue=data['name']
-            var tempValue=data["main"]['temp']
-            var descValue=data["weather"][0]['main']
-            var iconValue=data["weather"][0]['icon']
-            name1.innerHTML=nameValue
-            temp.innerHTML=Math.round(tempValue)+"°C"
-            desc.innerHTML=descValue
-            document.querySelector(".icon").src="http://openweathermap.org/img/wn/"+iconValue+"@2x.png"
-            console.log(data)
-        })
-
-
-    .catch(err=>alert("Wrong City!Enter a Correct City"))
-    fetch("https://pixabay.com/api/?key=31607599-89fa6d790daae6d87152ac3d8&per_page=200")
-        .then(res=>res.json())
-        .then(result=>{
-            console.log(result)
-            const imgae=result.hits[Math.round(Math.random() * 200)].largeImageURL
-            console.log(imgae)
-            document.body.style.backgroundImage=`url(${imgae})`
-            
-        })
+    WeatherApp()
+    
     
 })
-
